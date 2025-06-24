f0524298cd4ce042d5b180402970e6405952be55
Safal <080bei034.safal@pcampus.edu.np>
2025-06-24 11:41:55 +0545

    Added driver

--- driver.sv
+++ driver.sv
@@ -1,16 +1,30 @@
 class driver;
 
-virtual TopIf vif;
-mailbox #(transaction) gen2dri;
+    virtual TopIf vif;
+    mailbox #(transaction) gen2drv;
 
-function new(mailbox #(transaction) gen2dri , virtual TopIf vif) 
-    this.gen2dri = gen2dri;
-    this.vif =  vif;
-endfunction
+    function new(mailbox #(transaction) gen2drv , virtual TopIf vif); 
+        this.gen2drv = gen2drv;
+        this.vif = vif;
+    endfunction
 
-task run()
+    task run();
+        transaction t;
+        forever begin
+            gen2drv.get(t);
+            drive(t);
+        end
+    endtask
 
-endtask
+    task drive(ref transaction t);
+        vif.addr    <= t.addr;
+        vif.wr_data <= t.wr_data;
+        vif.wr_en   <= t.wr_en;
+        vif.rd_en   <= t.rd_en;
 
-
+        t.display();
+        // @(posedge vif.clk);
+        // vif.wr_en <= 0;
+        // vif.rd_en <= 0;
+    endtask
 endclass
